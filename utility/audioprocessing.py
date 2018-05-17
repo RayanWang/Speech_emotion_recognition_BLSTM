5e799dcd4cd8d3bd40dcac6a809510687582cf41
Rayan <shalo813.sw@gmail.com>
2018-05-17 10:51:36 +0800

    Bug fix.

--- utility/audioprocessing.py
+++ utility/audioprocessing.py
@@ -13,7 +13,7 @@ import time
 
 RATE = 16000
 CHUNK_DURATION_MS = 30  # supports 10, 20, 30 (ms)
-CHUNK_SIZE = int(RATE * CHUNK_DURATION_MS * 2 / 1000)  # chunk to read in bytes
+CHUNK_SIZE = int(RATE * CHUNK_DURATION_MS / 1000)  # chunk to read in samples
 NUM_WINDOW_CHUNKS = int(400 / CHUNK_DURATION_MS)  # 400ms / 30ms  frames
 NUM_WINDOW_CHUNKS_END = NUM_WINDOW_CHUNKS * 2
 
@@ -76,7 +76,7 @@ def voice_segmentation(filename, outdir):
         buffer_flags_end = [0] * NUM_WINDOW_CHUNKS_END
         buffer_index_end = 0
 
-        raw_data = array('b')
+        raw_data = array('h')
         index = 0
         start_point = 0
         start_time = time.time()
@@ -84,7 +84,7 @@ def voice_segmentation(filename, outdir):
         while not got_sentence and not ended:
             chunk = seg[(offset - CHUNK_DURATION_MS):offset].raw_data
 
-            raw_data.extend(array('b', chunk))
+            raw_data.extend(array('h', chunk))
             offset += CHUNK_DURATION_MS
             index += CHUNK_SIZE
             time_used = time.time() - start_time
@@ -105,7 +105,7 @@ def voice_segmentation(filename, outdir):
                 if num_voiced > 0.8 * NUM_WINDOW_CHUNKS:
                     sys.stdout.write(' Start sentence ')
                     triggered = True
-                    start_point = index - CHUNK_SIZE * 10  # start point
+                    start_point = index - CHUNK_SIZE * 20  # start point
             # end point detection
             else:
                 num_unvoiced = NUM_WINDOW_CHUNKS_END - sum(buffer_flags_end)
@@ -115,7 +115,7 @@ def voice_segmentation(filename, outdir):
                     got_sentence = True
 
             if offset >= len(seg):
-                print('File end')
+                sys.stdout.write(' File end ')
                 ended = True
 
             sys.stdout.flush()
